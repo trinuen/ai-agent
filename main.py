de0e2cfb74c20b8f81df7223838ddd110bac6e07
Tri Nguyen <tous0905@gmail.com>
2026-03-12 13:28:14 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import argparse
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', contents=messages
+    )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+if __name__ == "__main__":
+    main()
